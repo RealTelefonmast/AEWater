01425235aad012143bb893ffe308082b8ab6f262
Maxim <MaximSw@web.de>
2022-08-21 01:11:27 +0200

    Init

---
+++ Source/AEWater/AEWater/BiomeWorker_UnderwaterOcean.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace AEWater
+{
+    public class BiomeWorker_UnderwaterOcean : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+            if (tile.WaterCovered && tile.elevation <= 0)
+            {
+                return float.MaxValue;
+            }
+            return float.MinValue;
+        }
+    }
+}
---
+++ Source/AEWater/AEWater/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4ff6a3a-4310-4c21-a146-0b09904a8a63")]
